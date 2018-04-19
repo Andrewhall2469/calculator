c8d6fbf3721ea7182820f41be57bfc4183a3ac3d
AndrewHall2469 <AndrewHall978@gmail.com>
2018-04-19 11:43:09 +0100

    Added in the divide by zero test

--- src/test/java/com/qa/calculator/DividerTest.java
+++ src/test/java/com/qa/calculator/DividerTest.java
@@ -14,4 +14,12 @@ public class DividerTest {
 		int actualValue = divider.divide(9,3);
 		Assert.assertEquals(expectedValue, actualValue);
 	}
+	
+	@Test
+	public void zerotest() {
+		divider = new Divider();
+		int expectedValue = 0;
+		int actualValue = divider.divide(9,0);
+		Assert.assertEquals(expectedValue, actualValue);
+	}
 }

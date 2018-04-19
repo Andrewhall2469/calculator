de9924b30f17611790069be4fa001bafe83eea3f
AndrewHall2469 <AndrewHall978@gmail.com>
2018-04-19 10:30:56 +0100

    TDD - Multiplier Test Created

---
+++ src/test/java/com/qa/calculator/MultiplierTest.java
@@ -0,0 +1,20 @@
+package com.qa.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MultiplierTest {
+	
+	private Multiplier multiplier;
+
+	@Test
+	public void test() {
+		multiplier = new Multiplier();
+		int expectedValue = 16;
+		int actualValue = multiplier.subtract(4,4);
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+
+}
